f3032504c2b26ec60bc2afda9f176cce901e3733
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-05-26 11:52:21 -0400

    add Req decorator to handler to access request

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 //prefix for every route in this controller
 @Controller('cats')
 export class CatsController {
   @Get() //can add path information in the decorator to add to the route. example @Get('breed')
-  findAll(): string {
+  findAll(@Req() request: Request): string {
     return 'This action returns all cats!';
   }
 }
