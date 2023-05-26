30ca4affb0726af7394b99e92569f458d85e043a
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-05-26 13:08:24 -0400

    route for findOne cat, using route parameters

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, Post, Header } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Header, Param } from '@nestjs/common';
 
 //prefix for every route in this controller
 @Controller('cats')
@@ -12,5 +12,12 @@ export class CatsController {
   @Get() //can add path information in the decorator to add to the route. example @Get('breed')
   findAll(): string {
     return 'This action returns all cats!';
+  }
+
+  @Get(':id')
+  findOne(@Param() params: any): string {
+    //decorate the params method to make the route parameters available as properties of that decorated method parameter
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
   }
 }
