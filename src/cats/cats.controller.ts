8013156932d93d49dfe106233c466f2d72393423
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-05-26 11:57:38 -0400

    remove commented out example, added POST cats route

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Post } from '@nestjs/common';
 
 //prefix for every route in this controller
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
   @Get() //can add path information in the decorator to add to the route. example @Get('breed')
-  //   findAll(@Req() request: Request): string {
   findAll(): string {
     return 'This action returns all cats!';
   }
