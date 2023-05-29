04beff9dc198c4b5fcdb5fc5e5f4bcf9b58ec386
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-05-29 11:17:59 -0400

    joi schema

--- src/cats/create-cat.dto.ts
+++ src/cats/create-cat.dto.ts
@@ -1,4 +1,12 @@
-export class CreateCatDto {
+import Joi from 'joi';
+
+export const createCatSchema = Joi.object({
+  name: Joi.string().required(),
+  age: Joi.number().required(),
+  breed: Joi.string().required(),
+});
+
+export interface CreateCatDto {
   name: string;
   age: number;
   breed: string;
